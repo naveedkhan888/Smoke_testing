ea8f58a561bfc467df60ec9fa36aa707fd750a87
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-09-19 15:04:15 +0500

    sdfsdf

--- header.php
+++ header.php
@@ -36,7 +36,9 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<canvas class="tcg-smoke-cursor" id="tcg-smoke-cursor"></canvas>
+<?php if (!is_admin()) : ?>
+    <canvas class="tcg-smoke-cursor" id="tcg-smoke-cursor"></canvas>
+<?php endif; ?>
 <div id="page" class="site">
 
 <?php if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {

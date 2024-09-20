1acdeb1ba3406deb4146daf99a5cf2ace3ca6e26
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-09-20 15:32:05 +0500

    sdfsdf

--- functions.php
+++ functions.php
@@ -136,7 +136,7 @@ function restimo_fonts_url() {
 	}
 
 	if ( !isset( $second_font['font-family'] ) || $second_font['font-family'] == '' ) {
-		$font_families[] = 'Merriweather:300,400,500,600,700';
+		$font_families[] = 'Merriweather:300,400,500,600,700,700i';
 	}
 
 	if ( $font_families ) {

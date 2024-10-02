f919c3b26f078cc47357866551160c895d09326e
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-10-02 14:29:03 +0500

    sssssssss

--- inc/backend/elementor/widgets/image-box-2.php
+++ inc/backend/elementor/widgets/image-box-2.php
@@ -432,7 +432,9 @@ class Restimo_Image_Box_2 extends Widget_Base{
 			<?php echo wp_kses_post( $image_html ); ?>
 			<div class="content-box">
 				<?php if ( $settings['title'] ) { 
-				    echo wp_kses_post( $title_html . ' <i class="xp-webicon-trajectory"></i>' ); 
+				    // Append the icon inside the anchor tag
+				    $title_html = str_replace('</a>', ' <i class="xp-webicon-trajectory"></i></a>', $title_html);
+				    echo wp_kses_post( $title_html ); 
 				} ?>
 				<?php if( $settings['des'] ) { echo '<p>' .$settings['des']. '</p>'; } ?>
 			</div>
